ff91d1f3fc5a3ca9798287c0903240a5ee16811f
derek.coelho <derek.coelho.ext@vr.com.br>
2023-09-02 22:07:37 -0300

    fix: solved app name error

--- monetary_evolution/apps/monetary/apps.py
+++ monetary_evolution/apps/monetary/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 class MonetaryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "monetary"
+    name = "monetary_evolution.apps.monetary"
