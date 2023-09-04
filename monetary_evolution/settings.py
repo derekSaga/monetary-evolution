4a75681cc39fe0fe9bd638429d70b3ea8937af0b
derek.coelho <derek.coelho.ext@vr.com.br>
2023-09-04 10:21:31 -0300

    refactor: changed constants

--- monetary_evolution/settings.py
+++ monetary_evolution/settings.py
@@ -107,7 +107,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -135,3 +135,4 @@ DATABASES = {
 
 API_VAT_COMPLY = os.environ["API_VAT_COMPLY"]
 DATE_FORMAT = "%Y-%m-%d"
+RATES_DEFAULT = ["BRL", "EUR", "JPY"]
